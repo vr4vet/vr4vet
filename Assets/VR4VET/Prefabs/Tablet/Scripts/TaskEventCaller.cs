7982283b4b67389bfbbb85392fc52ef34b69b8b9
jorgeega <jorge.e.garcia@ntnu.no>
2022-08-24 18:19:42 +0200

    comment change

--- Assets/VR4VET/Prefabs/Tablet/Scripts/TaskEventCaller.cs
+++ Assets/VR4VET/Prefabs/Tablet/Scripts/TaskEventCaller.cs
@@ -1,7 +1,13 @@
+/* Copyright (C) 2022 IMTEL NTNU - All Rights Reserved
+ * Quick script to call the the task mananger methods " FerdighetManager" 
+ * its not possible to call them from the editor
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Tablet;
+
 public class TaskEventCaller : MonoBehaviour
 {
 
