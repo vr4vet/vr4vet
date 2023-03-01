88292943b6d577768a01bdfa2d5196a689303061
Jorge Garcia <jorge.e.garcia@ntnu.no>
2023-03-01 17:13:36 +0100

    deled more un-needed methods and replaced findDeepchild for references

---
+++ Assets/VR4VET/Components/Tablet/Scripts/SkillPage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Tablet
+{
+
+
+    public class SkillPage : MonoBehaviour
+    {
+
+            private void GenerateFerdighetPage(Skill skill)
+        {
+
+
+
+        }
+
+
+
+
+        
+    }
+
+
+}
