6f41d5c72c99124b899b387a4166a283e49df910
Jorge <jores10@hotmail.com>
2023-05-04 14:44:28 +0200

    Change how the points are stored in the data structure

--- Assets/VR4VET/Components/Tasks/Scripts/Step.cs
+++ Assets/VR4VET/Components/Tasks/Scripts/Step.cs
@@ -7,6 +7,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Task
 {
@@ -14,20 +15,16 @@ namespace Task
     [System.Serializable]
     public class Step 
     {
-        [HideInInspector]
-        public int archivedPoints;
 
-        [SerializeField]   private string _StepName;
+        [SerializeField]   private string _stepName;
         [SerializeField]   [Range(1, 20)] private int _repetionNumber = 1;    
-        [SerializeField] private int _valuePerRep;
 
-        private bool _compleated;
-        private List<Skill> _relatedSkills = new List<Skill>();
+        private bool _compleated=false;
         private int _repetionsCompleated = 0;
 
-        public int ValuePerRep { get => _valuePerRep; set => _valuePerRep = value; }
         public int RepetionNumber { get => _repetionNumber; set => _repetionNumber = value; }
         public int RepetionsCompleated { get => _repetionsCompleated; set => _repetionsCompleated = value; }
+        public string StepName { get => _stepName; set => _stepName = value; }
 
         public void CompleateRep()
         {
@@ -38,93 +35,44 @@ namespace Task
 
         }
 
-        public int MaxPoints()
+        public int CompleatedPercent()
         {
-            return _repetionNumber * _valuePerRep;
+            int porcent = _repetionsCompleated * 100 / _repetionNumber;
+            return porcent;
         }
 
-        public int Points()
-        {
-            return _repetionsCompleated * _valuePerRep;
-        }
-
-        public void CompleateAll()
+        public void CompleateAllReps()
         {
             _repetionsCompleated = _repetionNumber;
         }
 
-
-        public List<Skill> getRelatedSkills()
-        {
-            return _relatedSkills;
-        }
+     
 
 
         public bool IsCompeleted()
         {
+            if (_repetionNumber == _repetionsCompleated) _compleated = true;
             return _compleated;
         }
 
-
      
-        /// Mark this aktivitet as done
+        /// Mark this step as done
        
         public void SetCompleated(bool value)
         {
             _compleated = value;
         }
 
-
-
-        public string GetName()
-        {
-            return _StepName;
-        }
-
+  
 
         /// Set the name of this aktivitet
   
         public void SetAktivitetName(string value)
         {
-            _StepName = value;
+            _stepName = value;
         }
 
 
-
-        public int GetAchievedPoeng()
-        {
-            return archivedPoints;
-        }
-
-
-     
-        public void SetAchievedPoeng(int givenPoen)
-        {
-            archivedPoints = givenPoen;
-        }
-
-
-        /*
-        public void AddArchivedPoints(int value)
-        {
-            foreach (Skill ferdighet in _relatedSkills)
-            {
-                foreach (Step aktivitet in ferdighet.GetFerdighetAktiviteter().Keys.ToList())
-                {
-                    if (aktivitet.GetName() == _StepName)
-                    {
-                        //if (achievedPoeng + value < ferdighet.GetAchievedPoeng())
-                            archivedPoints += value;
-                        return;
-                        //else
-                        //    achievedPoeng = ferdighet.GetAchievedPoeng();
-                    }
-                }
-            }
-        }
-
-
-        */
 
     }
 }
