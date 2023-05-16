6b1bb96acc93c93833b60ca6c4fddfa1288d111f
Jorge <jores10@hotmail.com>
2023-05-16 16:10:30 +0200

    #104 changed the data structure of the task to fit current design, new features will be added in the future

--- Assets/VR4VET/Components/Tasks/Scripts/TaskHolder.cs
+++ Assets/VR4VET/Components/Tasks/Scripts/TaskHolder.cs
@@ -1,12 +1,9 @@
-/*
- * Developer: Jorge Garcia
+/* Developer: Jorge Garcia
  * Ask your questions on github: https://github.com/Jorest
  */
-using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
-
-
 
 namespace Task
 {
@@ -15,19 +12,14 @@ namespace Task
         public static TaskHolder Instance;
         private List<TaskxTarget> _taskAndTargerts = new List<TaskxTarget>();
 
-        [SerializeField]
-        private List<Skill> _skillList = new List<Skill>();
+        [Header("Profession Tasks")]
+        [SerializeField] public List<Skill> skillList = new List<Skill>();
 
-        [SerializeField]
-        private List<BTask> _tasks = new List<BTask>();
-        private List<Task> _taskRetro = new List<Task>();
-
-
+        [SerializeField] public List<Task> taskList = new List<Task>();
 
         //making the task holder a singleton
-        void Awake()
+        private void Awake()
         {
-            SetMaxValues();
             if (Instance == null)
             {
                 Instance = this;
@@ -39,55 +31,9 @@ namespace Task
             }
         }
 
-
-        public void SetMaxValues()
-        {
-            foreach (BTask task in _tasks)
-            {
-                foreach (Subtask sub in task.Subtasks)
-                {
-                    sub.addSubToSkill();
-                    sub.UpdateStepsReps();
-                }
-            }
-        }
-
-        public List<Skill> getSkillList()
-        {
-            List<Skill> returnskills = new List<Skill>();
-            foreach (Skill skill in _skillList)
-            {
-
-                returnskills.Add(skill);
-            }
-
-            return returnskills;
-        }
-
-
-        public List<BTask> GetTaskList()
-        {
-           
-            return _tasks;
-        }
-
-        public List<Task> GetRetroTaskList()
-        {
-
-
-            return _taskRetro;
-        }
-
-        // to think the points should just be a multiplier of the points. 
-
-
-
-
-
-
         public void CompleateSubtask(string taskName, string SubtaskName, bool compleateStepReps)
         {
-            foreach (BTask task in _tasks)
+            foreach (Task task in taskList)
             {
                 if (task.TaskName == taskName)
                 {
@@ -97,7 +43,7 @@ namespace Task
                         {
                             foreach (Step step in subtask.StepList)
                             {
-                               step.SetCompleated(true);                                                           
+                                step.SetCompleated(true);
                             }
                             Debug.LogError("Step not found in Subtask");
                         }
@@ -106,31 +52,12 @@ namespace Task
                 }
             }
         }
-
-
-        public void AddPointsToSkill(string stepName, string skillName, int points)
-        {
-
-
-            
-        }
-
-
-     
-
     }
-
-
-
-
-
 
     [System.Serializable]
     public class TaskxTarget
     {
         public Subtask subtask;
         public GameObject target;
-
     }
-
 }
