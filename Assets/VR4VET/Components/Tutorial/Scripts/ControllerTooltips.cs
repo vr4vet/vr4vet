90f721891807f35db16169563d4115138ee2803a
Snorrfo <snorre.forbregd@ntnu.no>
2023-06-21 15:13:31 +0200

    Added new models for Controller Tooltips

--- Assets/VR4VET/Components/Tutorial/Scripts/ControllerTooltips.cs
+++ Assets/VR4VET/Components/Tutorial/Scripts/ControllerTooltips.cs
@@ -10,50 +10,84 @@ public class ControllerTooltips : MonoBehaviour
     public Transform OVRMod;
     public Transform IndexMod;
     public Transform HTCMod;
+    public List<Transform> _oldModelR;
+    public List<Transform> _oldModelL;
     public bool EnableOnStart = false;
     private Transform ActiveL;
     private Transform ActiveR;
     private Transform _destinationL;
     private Transform _destinationR;
-    private List<Transform> _oldModelR;
-    private List<Transform> _oldModelL;
+    private bool _controllerIsActive = false;
     
     
     // Start is called before the first frame update
     void Start()
     {
+        if(!Player)
+        {
+            try
+            {
+            Player = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+            catch (UnassignedReferenceException)
+            {
+                Debug.LogError("Assign the player under the tooltip or with a Player tag");
+                throw;
+            }
+        }
+
+        // Get the location of the players head, and use that for player location moving forward.
         Player = Player.Find("PlayerController/CameraRig/TrackingSpace");
         _destinationL = Player.Find("LeftHandAnchor/LeftControllerAnchor/LeftController/ModelsLeft");
         _destinationR = Player.Find("RightHandAnchor/RightControllerAnchor/RightController/ModelsRight");
         OVRMod = transform.Find("OculusModels");
         IndexMod = transform.Find("IndexModels");
         HTCMod = transform.Find("HTCModels");
+        Player = Player.Find("CenterEyeAnchor");
 
-        if(InputBridge.Instance.IsValveIndexController)
+        if (ActiveControllers != null)
         {
-            // Load Index model
-            ActiveL = IndexMod.Find("LeftTTController");
-            ActiveR = IndexMod.Find("RightTTController");
-            ActiveControllers = IndexMod;
-        }
-        else if (InputBridge.Instance.IsHTCDevice)
-        {
-            // Load HTC model
-            ActiveL = HTCMod.Find("LeftTTController");
-            ActiveR = HTCMod.Find("RightTTController");
-            ActiveControllers = HTCMod;
+            ActiveL = ActiveControllers.Find("LeftTTController");
+            ActiveR = ActiveControllers.Find("RightTTController");
+            Debug.Log("Controllers overridden in editor");
+            Debug.Log(ActiveControllers);
         }
         else
         {
-            // Load OVR model
-            ActiveL = OVRMod.Find("LeftTTController");
-            ActiveR = OVRMod.Find("RightTTController");
-            ActiveControllers = OVRMod;
+            if(InputBridge.Instance.IsValveIndexController)
+            {
+                // Load Index model
+                ActiveL = IndexMod.Find("LeftTTController");
+                ActiveR = IndexMod.Find("RightTTController");
+                ActiveControllers = IndexMod;
+                Debug.Log("Found Index controllers");
+            }
+            else if(InputBridge.Instance.IsHTCDevice)
+            {
+                // Load HTC model
+                ActiveL = HTCMod.Find("LeftTTController");
+                ActiveR = HTCMod.Find("RightTTController");
+                ActiveControllers = HTCMod;
+                Debug.Log("Found HTC controllers");
+            }
+            else
+            {
+                // Load OVR model
+                ActiveL = OVRMod.Find("LeftTTController");
+                ActiveR = OVRMod.Find("RightTTController");
+                ActiveControllers = OVRMod;
+                Debug.Log("Found Oculus controllers, no controllers, or unsupported controllers");
+            }
         }
-        if (EnableOnStart) EnableControllers();
+        if(EnableOnStart)
+        {
+            SetActiveControllerTT();
+            
+        }
     }
 
-    public List<Transform> EnableControllers() {
+    public List<Transform> EnableTTController() {
+        _controllerIsActive = true;
         // Disable other hand models and save them for later use
         for(int i = 0; i < _destinationL.childCount; i++) {
             if (_destinationL.GetChild(i).gameObject.activeSelf) {
@@ -65,9 +99,9 @@ public class ControllerTooltips : MonoBehaviour
             if (_destinationR.GetChild(i).gameObject.activeSelf) {
                 _oldModelR.Add(_destinationR.GetChild(i));
                 _destinationR.GetChild(i).gameObject.SetActive(false);
-                Debug.Log(_destinationR.GetChild(i).gameObject.activeSelf);
             }
         }
+        // Set the controllers as the hands in hierarchy
         ActiveL.parent = _destinationL;
         ActiveL.localPosition = new Vector3(0, 0, 0);
         ActiveL.SetAsFirstSibling();
@@ -76,7 +110,40 @@ public class ControllerTooltips : MonoBehaviour
         ActiveR.SetAsFirstSibling();
         return new List<Transform>() {ActiveL, ActiveR};
     }
-    public void disable() {
+
+    public void SetActiveControllerTT(List<string> ActiveTooltips = null)
+    {
+        foreach (Transform TT in ActiveL.Find("ControllerParent"))
+        {
+            if (TT.name.Contains("Tooltip"))
+            {
+                TT.gameObject.SetActive(false);
+            }
+        }
+        foreach (Transform TT in ActiveR.Find("ControllerParent"))
+        {
+            if (TT.name.Contains("Tooltip"))
+            {
+                TT.gameObject.SetActive(false);
+            }
+        }
+
+        if(ActiveTooltips != null)
+        {
+            if(!_controllerIsActive)
+            {
+                EnableTTController();
+            }
+            foreach (string TT in ActiveTooltips)
+            {
+                ActiveL.Find("Controllerparent/" + TT + "Tooltip").gameObject.SetActive(true);
+                ActiveR.Find("Controllerparent/" + TT + "Tooltip").gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void DisableTTController() {
+        _controllerIsActive = false;
         _destinationL.Find("LeftTTController").parent = transform.Find("ActiveControllers");
         _destinationR.Find("RightTTController").parent = transform.Find("ActiveControllers");
         _oldModelL.ForEach((Model) => {
@@ -85,9 +152,5 @@ public class ControllerTooltips : MonoBehaviour
         _oldModelR.ForEach((Model) => {
             Model.gameObject.SetActive(true);
         });
-    }
-    // Update is called once per frame
-    void Update()
-    {
     }
 }
