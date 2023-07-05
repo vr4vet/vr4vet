82ecf5b4541fa9e8e4f4b3283546bcc1ddc3fa9f
Irtaza Khan <irtazakhan100@gmail.com>
2023-07-05 21:06:43 +0200

    Tablet Score info
    ScoreChartUI update

--- Assets/VR4VET/Components/Tablet/UI/Scripts/ScoreChartUI.cs
+++ Assets/VR4VET/Components/Tablet/UI/Scripts/ScoreChartUI.cs
@@ -1,88 +1,129 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ScoreChartUI : MonoBehaviour
 {
+    [SerializeField] private GameObject ChartTaskPrefab;
+    [SerializeField] private GameObject ChartSubtaskPrefab;
+    [SerializeField] private GameObject ChartSkillTextPrefab;
+    [SerializeField] private Transform taskRowParent;
+    [SerializeField] private Transform skillRowParent;
 
-    [SerializeReference] GameObject ChartTaskPrefab;
-    [SerializeReference] GameObject ChartSubtaskPrefab;
-    [SerializeReference] GameObject ChartSkillTextPrefab;
+    private int currentPage = 0;
+    private int pageSize = 1;
+    private List<Transform> skillColumns = new List<Transform>();
+    private List<Task.Task> allTasks = new List<Task.Task>();
+    private List<Task.Skill> allSkills = new List<Task.Skill>();
 
+    private RectTransform rectTransform;
+    private float leftLimit = -1000;
+    private float rightLimit = 0;
 
-    [SerializeReference] Transform taskRowParent;
-    [SerializeReference] Transform skillRowParent; //we dont assign to this, we assign to children of this
-    List<Transform> SkillColumns = new(); //each existing skill has it's own column
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        rectTransform = GetComponent<RectTransform>();
+        allTasks = Task.TaskHolder.Instance.taskList;
+        allSkills = Task.TaskHolder.Instance.skillList;
+        Generate(allTasks);
     }
-
-
-
-
-
-
 
     /// <summary>
     /// call this after task data initialization in Dynamic Data Displayer
     /// </summary>
     /// <param name="tasks"> all tasks </param>
-    void Generate(List<TaskData> tasks)
+    private void Generate(List<Task.Task> tasks)
     {
-        List<object> tasksAndSubtasks = new();
-        //get put tasks and subtasks into it
+        ClearUI();
 
-        foreach (var item in tasksAndSubtasks)
+        int totalTaskCount = tasks.Sum(task => task.Subtasks.Count) + tasks.Count;
+        int pageCount = Mathf.CeilToInt((float)totalTaskCount / pageSize);
+
+        currentPage = Mathf.Clamp(currentPage, 0, pageCount - 1);
+        int startIndex = currentPage * pageSize;
+        int endIndex = Mathf.Min(startIndex + pageSize, totalTaskCount);
+
+        int currentTaskIndex = 0;
+        int currentSubtaskIndex = 0;
+
+        foreach (var task in tasks)
         {
-            if (item.GetType().FullName == "TaskData")
+            if (currentTaskIndex >= startIndex && currentTaskIndex < endIndex)
             {
-                GameObject b = Instantiate(ChartTaskPrefab);
-                //add TaskUI here and let the player click it to goto the desired task/subtask
+                Instantiate(ChartTaskPrefab, taskRowParent).name = "T" + currentTaskIndex;
+            }
 
-                //instantiate task
-            }
-            else if (item.GetType().FullName == "SubTaskData")
-            {
-                //instantiate task
-                //add TaskUI here and let the player click it to goto the desired task/subtask
+            currentTaskIndex++;
 
-            }
-            else
+            foreach (var subtask in task.Subtasks)
             {
-                throw new System.Exception("ScoreChartUI @ Generate() - added unknown object to tasks&subtasks to instantiate ");
+                if (currentSubtaskIndex >= startIndex && currentSubtaskIndex < endIndex)
+                {
+                    Instantiate(ChartSubtaskPrefab, taskRowParent).name = "S" + currentSubtaskIndex;
+                }
+
+                currentSubtaskIndex++;
+                if (currentSubtaskIndex >= endIndex)
+                    break;
             }
+
+            if (currentTaskIndex >= endIndex)
+                break;
         }
 
 
+        int skillStartIndex = currentPage * pageSize;
+        int skillEndIndex = Mathf.Min(skillStartIndex + pageSize, allSkills.Count);
 
-        //get tasks, instantiate task-subtask(1,2,3,4...etc) row
-        //get skills, instantiate skill row (Skill name + skill text for each task/subtask)
-
-
-
-
-
+        for (int i = skillStartIndex; i < skillEndIndex; i++)
+        {
+            Instantiate(ChartSkillTextPrefab, skillRowParent).name = i.ToString();
+        }
     }
 
 
-
-
-    // Update is called once per frame
-    void Update()
+    private void ClearUI()
     {
+        foreach (Transform child in taskRowParent)
+            Destroy(child.gameObject);
 
+        foreach (Transform child in skillRowParent)
+            Destroy(child.gameObject);
+
+        skillColumns.Clear();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) PageUp();
+        if (Input.GetKeyDown(KeyCode.S)) PageDown();
+        if (Input.GetKeyDown(KeyCode.A)) PageLeft();
+        if (Input.GetKeyDown(KeyCode.D)) PageRight();
+    }
 
+    public void PageDown()
+    {
+        currentPage++;
+        Generate(allTasks);
+    }
 
+    public void PageRight()
+    {
+        float newPosition = rectTransform.anchoredPosition.x - 150;
+        newPosition = Mathf.Clamp(newPosition, leftLimit, rightLimit);
+        rectTransform.anchoredPosition = new Vector2(newPosition, rectTransform.anchoredPosition.y);
+    }
 
+    public void PageLeft()
+    {
+        float newPosition = rectTransform.anchoredPosition.x + 150;
+        newPosition = Mathf.Clamp(newPosition, leftLimit, rightLimit);
+        rectTransform.anchoredPosition = new Vector2(newPosition, rectTransform.anchoredPosition.y);
+    }
 
-
-
-
-
-
-
+    public void PageUp()
+    {
+        currentPage--;
+        Generate(allTasks);
+    }
 }
