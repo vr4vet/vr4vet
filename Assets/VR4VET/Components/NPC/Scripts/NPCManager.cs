1fa67126d8d64f3eebf21b4100c8ff57c85e0d81
jatobro <jakob.tossebro@gmail.com>
2023-09-27 12:19:35 +0200

    method for generating tree from json

--- Assets/VR4VET/Components/NPC/Scripts/NPCManager.cs
+++ Assets/VR4VET/Components/NPC/Scripts/NPCManager.cs
@@ -4,20 +4,20 @@ public class NPCManager : MonoBehaviour
 {
     public DialogueTree dialogueTree;
     public GameObject collidingObject;
+    public TextAsset data;
     
     private DialogueBoxController controller;
 
     // Start is called before the first frame update
     void Start()
-    {   
+    {    
+        GetTreeFromJson(data);
         controller = gameObject.AddComponent<DialogueBoxController>();
         controller.StartDialogue(dialogueTree, 0, "NPC");
     }
 
     private void OnTriggerEnter(Collider other)
     {   
-
-        
         if (other.gameObject.Equals(collidingObject)) 
         {
             Debug.Log(dialogueTree.sections[0].dialogue[0]);
@@ -25,9 +25,8 @@ public class NPCManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetTreeFromJson(TextAsset data) 
     {
-        
+        JsonUtility.FromJsonOverwrite(data.text, dialogueTree);
     }
 }
