8de9aa37849c2dc39a371b11fe56baaae65859c3
Martineem <martine.mansaker@gmail.com>
2023-09-27 11:55:57 +0200

    #62 Create test scene with working dialogue tree functionality. Co-authored-by: Jakob Tøssebro <jakob.tossebro@gmail.com> Co-authored-by: Simen Seeberg-Rommetveit <simensee@users.noreply.github.com>

--- Assets/VR4VET/Components/NPC/Scripts/NPCManager.cs
+++ Assets/VR4VET/Components/NPC/Scripts/NPCManager.cs
@@ -7,27 +7,25 @@ public class NPCManager : MonoBehaviour
     
     private DialogueBoxController controller;
 
+    float _time;
+
     // Start is called before the first frame update
     void Start()
     {   
-        controller = gameObject.AddComponent<DialogueBoxController>();
-        controller.StartDialogue(dialogueTree, 0, "NPC");
+        controller = gameObject.GetComponent<DialogueBoxController>();
+        _time = 0;
     }
 
     private void OnTriggerEnter(Collider other)
     {   
-
-        
         if (other.gameObject.Equals(collidingObject)) 
         {
-            Debug.Log(dialogueTree.sections[0].dialogue[0]);
-            Destroy(gameObject);
+            controller.StartDialogue(dialogueTree, 0, "NPC");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
