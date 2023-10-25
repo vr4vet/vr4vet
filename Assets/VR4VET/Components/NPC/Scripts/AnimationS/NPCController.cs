f7a949a6273da4edfc32bf6bdc607ce40f59aacd
Simen Seeberg-Rommetveit <simensee@gmail.com>
2023-10-25 12:05:08 +0200

    #210 resolve merge conflict

--- Assets/VR4VET/Components/NPC/Scripts/AnimationS/NPCController.cs
+++ Assets/VR4VET/Components/NPC/Scripts/AnimationS/NPCController.cs
@@ -9,22 +9,6 @@ using UnityEngine.AI;
 
 public class NPCController: MonoBehaviour
 {
-<<<<<<< Updated upstream
-
-    // Video Animation + movement
-    // https://www.youtube.com/watch?v=TpQbqRNCgM0
-    // Video Brackeys + NavMeshAgent etc.
-    // https://www.youtube.com/watch?v=xppompv1DBg&list=PLPV2KyIb3jR4KLGCCAciWQ5qHudKtYeP7&index=11
-    public float lookRadius = 8f;
-
-    public float personalSpaceFactor = 4f;
-    Transform target;
-    NavMeshAgent agent;
-
-    Animator animator;
-
-
-=======
     public float lookRadius = 8f;
     public float personalSpaceFactor = 4f;
     public bool shouldFollow = false;  // New parameter to control NPC's behavior.
@@ -33,7 +17,6 @@ public class NPCController: MonoBehaviour
     NavMeshAgent agent;
     Animator animator;
 
->>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
@@ -45,44 +28,6 @@ public class NPCController: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
-        float distance = Vector3.Distance(target.position, transform.position);
-        
-        // If the player is far away, go closer, but let them have some personal space. 
-        if (distance >= lookRadius) {
-            Vector3 direction = (target.position - transform.position).normalized;
-            agent.SetDestination(target.position - direction * personalSpaceFactor);
-            // print("destination: " + (target.position - personalSpaceCricle));
-            // when the navMeshAgent has angular speed 1200 (aka. turns instantly)
-            // we only walk forward
-            animator.SetFloat("VelocityY", agent.velocity.magnitude);
-
-            // when the navMeshAgent has angular speed 0 (aka. everything is controlled by the animation)
-            // We can walk backwards
-            // animator.SetFloat("VelocityX", agent.velocity.x);
-            // animator.SetFloat("VelocityY", agent.velocity.z);
-
-            // Debug stuff
-            // print("target.pos: " + target.position);
-            // print("mag: " + agent.velocity.magnitude);
-            // print("x: " + agent.velocity.x);
-            // print("z: " + agent.velocity.z);
-        } else {
-            // when the navMeshAgent has angular speed 1200 (aka. turns instantly)
-            // we only walk forward
-            animator.SetFloat("VelocityY", agent.velocity.magnitude);
-
-            // when the navMeshAgent has angular speed 0 (aka. everything is controlled by the animation)
-            // We can walk backwards
-            // animator.SetFloat("VelocityX", agent.velocity.x);
-            // animator.SetFloat("VelocityY", agent.velocity.z);
-        }
-    }
-
-
-    // visulaization of personal space and lookRadius
-    void OnDrawGizmosSelected() {
-=======
         Debug.Log("Should Follow: " + shouldFollow);
         float distance = Vector3.Distance(target.position, transform.position);
         
@@ -112,7 +57,6 @@ public class NPCController: MonoBehaviour
     // Visualization of personal space and lookRadius
     void OnDrawGizmosSelected() 
     {
->>>>>>> Stashed changes
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
         Gizmos.color = Color.blue;
