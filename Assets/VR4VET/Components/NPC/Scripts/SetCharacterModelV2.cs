7ac86fdc5bf5db34a0fae2ac0a4593d88aa8985d
LisBirUll <liseuliseu1@gmail.com>
2023-11-18 20:32:31 +0100

    super messy, not working, first draft

--- Assets/VR4VET/Components/NPC/Scripts/SetCharacterModelV2.cs
+++ Assets/VR4VET/Components/NPC/Scripts/SetCharacterModelV2.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Meta.WitAi;
 using Photon.Voice;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class SetCharacterModelV2 : MonoBehaviour
 {
@@ -25,6 +27,7 @@ public class SetCharacterModelV2 : MonoBehaviour
     [HideInInspector] private FollowThePlayerControllerV2 followThePlayerControllerV2;
     [HideInInspector] private DialogueBoxController dialogueBoxController;
 
+    // animation stuff
     [HideInInspector] private Animator animator; 
     [HideInInspector] private int isTalkingHash;
     [HideInInspector] private int hasNewDialogueOptionsHash;
@@ -35,6 +38,16 @@ public class SetCharacterModelV2 : MonoBehaviour
     [HideInInspector] private bool hasNewDialogueOptions;
     [HideInInspector] private float velocityY;
     [HideInInspector] private float velocityX;
+    
+    // Animation constraints
+    [HideInInspector] private RigBuilder rigBuilder;
+    [HideInInspector] private AnimationSpeakController animationSpeakController;
+    [SerializeField] private RigLayer targetTracking;
+    [SerializeField] private GameObject targetTrackingPrefab;
+    [SerializeField] private GameObject[] ConstrainedObjects;
+    [SerializeField] private GameObject SourceObject;
+
+    [SerializeField] private GameObject mixamoRigParent;
 
 
 
@@ -86,11 +99,18 @@ public class SetCharacterModelV2 : MonoBehaviour
         bonesAndSkin.transform.rotation = Quaternion.Euler(new Vector3(parentObject.transform.eulerAngles.x, parentObject.transform.eulerAngles.y, parentObject.transform.eulerAngles.z));
         //bonesAndSkin.transform.Rotate(new Vector3(parentObject.transform.rotation.x, parentObject.transform.rotation.y, parentObject.transform.rotation.z));
 
+        targetTrackingPrefab.transform.SetParent(bonesAndSkin.transform);
+
+        String mixamoString = mixamoRigParent.name.Split(":")[0];
+        Debug.Log(mixamoString);
+        GameObject conGameObject = GameObject.Find("mixamorig:Spine");
+        Debug.Log("ConstrainedGame object is: " + conGameObject);
+
         animator = bonesAndSkin.GetComponent<Animator>();
         if (animator == null) {
             Debug.Log("Adding new Animator");
-            bonesAndSkin.AddComponent<Animator>();
-            animator = bonesAndSkin.GetComponent<Animator>();
+            animator = bonesAndSkin.AddComponent<Animator>();
+            //animator = bonesAndSkin.GetComponent<Animator>();
         }
         animator.runtimeAnimatorController = runtimeAnimatorController;
         animator.avatar = avatar;
@@ -100,6 +120,34 @@ public class SetCharacterModelV2 : MonoBehaviour
         animator.SetFloat(velocityXHash, velocityX);
         animator.SetFloat(velocityYHash, velocityY);
         updateOtherScripts(animator);
+        // The rigbuilder
+        rigBuilder = bonesAndSkin.GetComponent<RigBuilder>();
+        if (rigBuilder == null) {
+            rigBuilder = bonesAndSkin.AddComponent<RigBuilder>();
+        }
+        rigBuilder.layers.Clear();
+        rigBuilder.layers.Add(targetTracking);
+        
+        MultiAimConstraint[] constraints = targetTracking.rig.GetComponentsInChildren<MultiAimConstraint>();
+        int i = 0;
+        foreach (MultiAimConstraint con in constraints)
+        {
+            if (i >= ConstrainedObjects.Length) {
+                Debug.Log("The RigLayer has more contraints than you have specified in contrainedObjects");
+            } else {
+                con.data.constrainedObject = conGameObject.transform;
+                //con.data.sourceObjects = new WeightedTransformArray{new WeightedTransform(targetRef.transform, 1)};
+                i++;
+            }
+        }
+        rigBuilder.Build();
+
+        if (!bonesAndSkin.TryGetComponent<AnimationSpeakController>(out AnimationSpeakController animationSpeakController))
+        {
+            bonesAndSkin.AddComponent<AnimationSpeakController>();
+            // The script also adds the source componenet
+        }
+
         //updateOtherScripts();
     }
 
@@ -130,4 +178,34 @@ public class SetCharacterModelV2 : MonoBehaviour
         dialogueBoxController.updateAnimator(animator);
         followThePlayerControllerV2.updateAnimator(animator);
     }
+
+
+    // void buildRig() {
+    //     // create rig gameobject
+    //     GameObject rigGameobject = new GameObject("Rig");
+    //     rigGameobject.transform.SetParent(transform, true);
+ 
+    //     // Add constraint to rig
+    //     MultiAimConstraint constraint = rigGameobject.AddComponent<MultiAimConstraint>();
+    //     constraint.Reset();
+    //     constraint.data.constrainedObject = ConstrainedObject.transform;
+    //     WeightedTransformArray sources = new WeightedTransformArray();
+    //     sources.Add(new WeightedTransform(SourceObject.transform, 1f));
+    //     constraint.data.sourceObjects = sources;
+ 
+    //     // Add the rig
+    //     Rig rig = rigGameobject.AddComponent<Rig>();
+ 
+    //     // Add rig to rigbuilder
+    //     rigBuilder = gameObject.AddComponent<RigBuilder>();
+    //     rigBuilder.layers.Clear();
+    //     rigBuilder.layers.Add(new RigLayer())
+    //     rigBuilder.layers.Add(new RigBuilder.RigLayer(rig, true));
+ 
+    //     // build rig graph (Does not seems to be doing anything)
+    //     rigBuilder.Build();
+    // }
+
+
+
 }
