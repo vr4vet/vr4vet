a07b2f72eb2bcdd79bf850f709fe22027bd5df9c
Simen Seeberg-Rommetveit <simensee@gmail.com>
2023-11-20 17:33:25 +0100

    change to correct camera pointer

--- Assets/VR4VET/Components/NPC/Scripts/TestSceneScripts/RedPlatformReact.cs
+++ Assets/VR4VET/Components/NPC/Scripts/TestSceneScripts/RedPlatformReact.cs
@@ -7,25 +7,13 @@ public class RedPlatformReact : MonoBehaviour
     private GameObject taskNPC;
     [SerializeField] public DialogueTree[] dialogueTrees; 
 
-    private ConversationController conversationController;
     
 
     void OnTriggerEnter(Collider other) {
-        // print the name of the object that collided with the trigger
-        Rigidbody otherRigidbody = other.attachedRigidbody;
-
-        if (otherRigidbody != null) {
-            // If there is a Rigidbody, print the name of the GameObject it's attached to
-            Debug.Log("Rigidbody GameObject Name: " + otherRigidbody.gameObject.name);
-        } else {
-            // If there is no Rigidbody, you can still print the name of the collider's GameObject
-            Debug.Log("Collider GameObject Name: " + other.gameObject.name);
-        }
+        
         if (!taskNPC) {
             Debug.Log("RedPlatformReact is triggered");
             taskNPC = eventTrigger.SpawnTaskNPC();
-            // conversationController = taskNPC.GetComponentInChildren<ConversationController>();
-            // conversationController?.insertDialogueTreeAndChange(dialogueTrees[0]);
         }
     }
 }
