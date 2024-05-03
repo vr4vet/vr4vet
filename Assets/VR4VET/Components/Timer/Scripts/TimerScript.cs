dd3d0db1d3e5d98a309eb36e7384aba1bfd75c92
frequin <frederik.friquin@gmail.com>
2024-05-03 12:47:02 +0200

    Added TimerScript to Task namespace and made prefabs

--- Assets/VR4VET/Components/Timer/Scripts/TimerScript.cs
+++ Assets/VR4VET/Components/Timer/Scripts/TimerScript.cs
@@ -5,36 +5,36 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using TMPro;
+namespace Task {
+    public class TimerScript : MonoBehaviour
+    {
 
-public class TimerScript : MonoBehaviour
-{
-
-    public IEnumerator StartTimer (int Timer, TMP_Text TimerDiplay) {
-        for (int i = Timer; i >= 0; i--){
-            TimeSpan RemainingTime = new TimeSpan(0, 0, i);
-            TimerDiplay.text = RemainingTime.ToString(@"mm\:ss");
-            yield return new WaitForSeconds(1f);
+        public IEnumerator StartTimer (int Timer, TMP_Text TimerDiplay) {
+            for (int i = Timer; i >= 0; i--){
+                TimeSpan RemainingTime = new TimeSpan(0, 0, i);
+                TimerDiplay.text = RemainingTime.ToString(@"mm\:ss");
+                yield return new WaitForSeconds(1f);
+            }
         }
-    }
-    public IEnumerator StartTimer (TMP_Text TimerDiplay, bool CountUp) {
-        TimeSpan Timer = new TimeSpan(0, 0, 0);
-        while(CountUp) {
-            Timer += new TimeSpan(0, 0, 1);
-            TimerDiplay.text = Timer.ToString(@"mm\:ss");
-            yield return new WaitForSeconds(1f);
+        public IEnumerator StartTimer (TMP_Text TimerDiplay, bool CountUp) {
+            TimeSpan Timer = new TimeSpan(0, 0, 0);
+            while(CountUp) {
+                Timer += new TimeSpan(0, 0, 1);
+                TimerDiplay.text = Timer.ToString(@"mm\:ss");
+                yield return new WaitForSeconds(1f);
+            }
         }
-    }
 
 #if UNITY_EDITOR
-    [SerializeField] private TMP_Text TimerDiplay;
-    [SerializeField] private int Timer;
-    [SerializeField] private bool CountUp;
-    public void StartTimer(){
-        if (!CountUp) {
-            StartCoroutine(StartTimer(Timer, TimerDiplay));
-        } else {
-            StartCoroutine(StartTimer(TimerDiplay, CountUp));
+        [SerializeField] private int Timer;
+        [SerializeField] private bool CountUp;
+        public void StartTimer(TMP_Text TimerDiplay){
+            if (!CountUp) {
+                StartCoroutine(StartTimer(Timer, TimerDiplay));
+            } else {
+                StartCoroutine(StartTimer(TimerDiplay, CountUp));
+            }
         }
-    }
 #endif    
+    }
 }
