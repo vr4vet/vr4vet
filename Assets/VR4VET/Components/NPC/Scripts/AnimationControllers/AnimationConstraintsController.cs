83aec00921e3a6a5b96b31583636df0d4001bb03
AkselLessum <alessum@hotmail.no>
2024-07-17 12:03:33 +0200

    #231 spine now also rotates slightly towards player to make NPC seem more natural

--- Assets/VR4VET/Components/NPC/Scripts/AnimationControllers/AnimationConstraintsController.cs
+++ Assets/VR4VET/Components/NPC/Scripts/AnimationControllers/AnimationConstraintsController.cs
@@ -10,9 +10,9 @@ public class AnimationConstraintsController : MonoBehaviour
     [HideInInspector] private Animator animator;
     [HideInInspector] private int isTalkingHash;
     [HideInInspector] private RigBuilder rigBuilder; // Use RigBuilder instead of Rig
-    [HideInInspector] private MultiAimConstraint multiAimConstraint;
-
     private GameObject targetRef;
+    private MultiAimConstraint headCon;
+    private MultiAimConstraint spineCon;
 
     /// <summary>
     /// Check that every compontent need exists
@@ -40,46 +40,47 @@ public class AnimationConstraintsController : MonoBehaviour
                 Rig rig = rigLayer.rig;
 
                 if (rig != null)
-                {
-                    // Access the constraints within the Rig
-                    multiAimConstraint = rig.GetComponentsInChildren<MultiAimConstraint>(true).FirstOrDefault();
-
-                    if (multiAimConstraint == null)
+                {                                                          
+                    targetRef = NPCToPlayerReferenceManager.Instance.PlayerTarget;
+                    if (targetRef != null)
                     {
-                        Debug.LogError("MultiAimConstraint not found in the 'TargetTracking' Rig Layer.");
-                    }
-                    else
-                    {
-                        targetRef = NPCToPlayerReferenceManager.Instance.PlayerTarget;
-                        if (targetRef != null)
+                        // Adds contraints at runtime
+                        MultiAimConstraint[] constraints = rig.GetComponentsInChildren<MultiAimConstraint>();
+                        foreach (MultiAimConstraint con in constraints)
                         {
-                            // Adds contraints at runtime
-                            MultiAimConstraint[] constraints = rig.GetComponentsInChildren<MultiAimConstraint>();
-                            foreach (MultiAimConstraint con in constraints)
-                            {
-                                // Set the player camera as the source object (what the NPC will look at)
-                                var sourceObject = con.data.sourceObjects;
-                                var newSource = new WeightedTransform(targetRef.transform, 1.0f);
-                                sourceObject.Add(newSource);
-                                con.data.sourceObjects = sourceObject;
+                            // Set the player camera as the source object (what the NPC will look at)
+                            var sourceObject = con.data.sourceObjects;
+                            var newSource = new WeightedTransform(targetRef.transform, 1.0f);
+                            sourceObject.Add(newSource);
+                            con.data.sourceObjects = sourceObject;
 
-                                // Manage settings for the constrained object(s)
-                                con.data.aimAxis = MultiAimConstraintData.Axis.Z;
-                                con.data.upAxis = MultiAimConstraintData.Axis.Y;
-                                con.data.constrainedXAxis = true;
-                                con.data.constrainedYAxis = true;
-                                con.data.constrainedZAxis = true;
+                            // Manage settings for the constrained object(s)
+                            con.data.aimAxis = MultiAimConstraintData.Axis.Z;
+                            con.data.upAxis = MultiAimConstraintData.Axis.Z;
+                            con.data.constrainedXAxis = true;
+                            con.data.constrainedYAxis = true;
+                            con.data.constrainedZAxis = true;
+                            if (con.gameObject.name == "AimObjectHead") {
+                                //con.data.constrainedXAxis = true;
                                 // The max ranges for how far to the side the NPC will look
                                 con.data.limits = new Vector2(-70f, 70f);
-
+                                headCon = con;
+                            } else if (con.gameObject.name == "AimObjectSpine") {
+                                // Spine should move less than head and not bend backwards (x-axis)
+                                con.data.limits = new Vector2(-40f, 40f);
+                                spineCon = con;
                             }
-                            rigBuilder.Build();
+                                
+                                
+
                         }
-                        else
-                        {
-                            Debug.LogError("Cannot find XR Rig Advanced/PlayerController/CameraRig in the scene");
+                        rigBuilder.Build();
                         }
+                    else
+                    {
+                        Debug.LogError("Cannot find XR Rig Advanced/PlayerController/CameraRig in the scene");
                     }
+                    
                 }
                 else
                 {
@@ -115,19 +116,21 @@ public class AnimationConstraintsController : MonoBehaviour
                 Vector3 forward = transform.forward;
                 // Angle between player and NPC
                 float angle = Vector3.Angle(forward, playerDirection);
-                Debug.Log(angle);
                 if (angle <= 90f)  {
                     // Enable the multi-aim constraint when character is talking and player not behind NPC
-                    multiAimConstraint.weight += 0.004f;
+                    if (headCon.weight < 0.7f) { headCon.weight += 0.004f; }
+                    if (spineCon.weight < 0.3f) { spineCon.weight += 0.004f; }
                 } else {
                     // If behind NPC, stop looking at player
-                    multiAimConstraint.weight -= 0.002f;
+                    headCon.weight -= 0.002f;
+                    spineCon.weight -= 0.002f;
                 }
             }
             else
             {
                 // Disable the multi-aim constraint when character is not talking
-                multiAimConstraint.weight -= 0.002f;
+                headCon.weight -= 0.002f;
+                spineCon.weight -= 0.002f;
             }
         } else {
             animator = GetComponentInChildren<Animator>();
--- Assets/VR4VET/Components/NPC/Scripts/SetCharacterModel.cs
+++ Assets/VR4VET/Components/NPC/Scripts/SetCharacterModel.cs
@@ -119,22 +119,37 @@ public class SetCharacterModel : MonoBehaviour
             Rig rig = rigObject.AddComponent<Rig>();
             rigBuilder.layers.Add(new RigLayer(rig));
             // Add last object to containt multi-aim constraints
-            GameObject aimObject = new GameObject("AimObject");
-            aimObject.transform.parent = rigObject.transform;
-            // Add multi-aim constraints and find the NPC head to make it the constrained object
-            MultiAimConstraint constraints = aimObject.AddComponent<MultiAimConstraint>();
+            GameObject aimObjectHead = new GameObject("AimObjectHead");
+            GameObject aimObjectSpine = new GameObject("AimObjectSpine");
+            aimObjectHead.transform.parent = rigObject.transform;
+            aimObjectSpine.transform.parent = rigObject.transform;
+
+            // Add multi-aim constraints components
+            MultiAimConstraint constraintsHead = aimObjectHead.AddComponent<MultiAimConstraint>();
+            MultiAimConstraint constraintsSpine = aimObjectSpine.AddComponent<MultiAimConstraint>();
             // Find the NPC head
             Component[] bodyParts = _bonesAndSkin.GetComponentsInChildren<Component>(true);
             GameObject NPCHead = null;
+            GameObject NPCSpine = null;
             foreach (Component comp in bodyParts) {
                 // Return component if name contains "Head"
-                if (comp.gameObject.name.Contains(":Head")) { NPCHead = comp.gameObject; break;}
+                if (comp.gameObject.name.Contains(":Head") && NPCHead == null) { NPCHead = comp.gameObject; }
+                // Return component if name contains "Spine"
+                if (comp.gameObject.name.Contains(":Spine2") && NPCSpine == null) {NPCSpine = comp.gameObject; }
+                // Stop checking body parts if both are found
+                if (NPCHead != null && NPCSpine != null) { break; }
             }
             if (NPCHead == null) {
                 Debug.LogError("Could not find NPC head");
             } else {
                 // Set constrained object to NPC head
-                constraints.data.constrainedObject = NPCHead.transform;
+                constraintsHead.data.constrainedObject = NPCHead.transform;
+            }
+            if (NPCSpine == null) {
+                Debug.LogError("Could not find NPC spine");
+            } else {
+                // Set constrained object to NPC spine
+                constraintsSpine.data.constrainedObject = NPCSpine.transform;
             }
 
             // If there is already a animation constraints controller (f.ex from previous model) remove it
@@ -143,7 +158,7 @@ public class SetCharacterModel : MonoBehaviour
             }
             // Add the constraints add runtime so the rig builds with the correct model
             _bonesAndSkin.AddComponent<AnimationConstraintsController>();
-            rigBuilder.Build();
+            
             Debug.Log("Rigbuilder instantiated and configurated for NPC");
 
         } else {
