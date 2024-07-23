6a4848c950a0c062c86f5182ceaa08c015d034fd
AkselLessum <alessum@hotmail.no>
2024-07-23 12:17:23 +0200

    59 added example script to check functionality

--- Assets/VR4VET/Components/NPC/Scripts/TestSceneScripts/FredrikSpecializedScript.cs
+++ Assets/VR4VET/Components/NPC/Scripts/TestSceneScripts/FredrikSpecializedScript.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class FredrikSpecializedScript : MonoBehaviour
 {
+    [HideInInspector] private NPCSpawner _npcSpawner;
+    [HideInInspector] private GameObject _npc;
+    private GameObject platform;
+    private ConversationController _conversationController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _npcSpawner = GameObject.Find("NPCSpawner").GetComponent<NPCSpawner>();
+        if (_npcSpawner == null) {
+            Debug.Log("Cant find spawner");
+        }
+        _npc = _npcSpawner._npcInstances[0];
+        platform = GameObject.Find("CommentPlatform");
+        _conversationController = _npc.GetComponentInChildren<ConversationController>();
+    }
+
+    void OnTriggerEnter() {
+        Debug.Log("Entered platform trigger");
+        _conversationController.CommentTrigger();
     }
 
     // Update is called once per frame
