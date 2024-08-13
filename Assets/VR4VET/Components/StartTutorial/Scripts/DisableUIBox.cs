d5e86a4d7a886d378a7c5e3929b904265f1b6313
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2024-08-13 17:20:06 +0200

    fix: fixed localization for StartTutorial and ui box #272

---
+++ Assets/VR4VET/Components/StartTutorial/Scripts/DisableUIBox.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableUIBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+}
