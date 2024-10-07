aca3f69fe814d54199f25b690721f8b3700c0ecb
johangg <johsgronstad@gmail.com>
2024-10-07 13:35:50 +0200

    Changed contextPrompt to be  a text box in inspector
    In AIConversationController script
    
    Issue Number: #304

--- Assets/VR4VET/Components/NPC/AI/AIConversationController.cs
+++ Assets/VR4VET/Components/NPC/AI/AIConversationController.cs
@@ -6,8 +6,12 @@ public class AIConversationController : MonoBehaviour
 {
 
     [SerializeField] private SaveUserSpeech _SaveUserSpeech;
+    
+    [TextArea(3, 10)]
     public string contextPrompt;
     public int maxTokens = 50;
+
+    
 
     void Start()
     {
