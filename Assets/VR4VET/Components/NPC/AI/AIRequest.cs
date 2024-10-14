26d1219dadd53e877fdb87e514139125ccdcba65
Rune Seeland <runesee@ntnu.stud.no>
2024-10-14 14:27:41 +0200

    Add thinking nod animation
    Add small nodding animation while awaiting OpenAI response. Slightly
    adjust wave animation length.
    
    Issue Number: #325
    
    Co-authored-by: nicoljn19 <nicoljn19@gmail.com>

--- Assets/VR4VET/Components/NPC/AI/AIRequest.cs
+++ Assets/VR4VET/Components/NPC/AI/AIRequest.cs
@@ -130,6 +130,7 @@ public class AIRequest : MonoBehaviour
                     Coroutine thinking = StartCoroutine(_dialogueBoxController.DisplayThinking());
                     yield return new WaitUntil(() => _AIResponseToSpeech.readyToAnswer);
                     StopCoroutine(thinking);
+                    _dialogueBoxController.stopThinking();
 
                     // Display the response in the dialogue box
                     StartCoroutine(_dialogueBoxController.DisplayResponse(responseText));
