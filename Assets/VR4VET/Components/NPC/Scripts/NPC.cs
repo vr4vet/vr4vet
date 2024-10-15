c6e0c10be6df910c17f000befccab0d14513052a
johangg <johsgronstad@gmail.com>
2024-10-15 22:04:17 +0200

    Hide duplicates in inspector
    To improve developer experience.
    
    Issue Number: #322

--- Assets/VR4VET/Components/NPC/Scripts/NPC.cs
+++ Assets/VR4VET/Components/NPC/Scripts/NPC.cs
@@ -14,8 +14,12 @@ public class NPC : ScriptableObject
     public GameObject NpcPrefab;
     public GameObject CharacterModel;
     public Avatar CharacterAvatar;
+    [HideInInspector]
     public TTSProvider selectedTTSProvider; // Dropdown for TTS provider
+    [HideInInspector]
     public int WitVoiceId; // Voice ID for Wit
+    [HideInInspector]
+
     public string OpenAiVoiceId; // Voice ID for OpenAI
     public Vector3 SpawnPosition;
     public Vector3 SpawnRotation;
@@ -31,6 +35,6 @@ public class NPC : ScriptableObject
 
     [TextArea(3, 10)]
     public string contextPrompt;
-    public int maxTokens=1000;
-    
+    public int maxTokens = 1000;
+
 }
