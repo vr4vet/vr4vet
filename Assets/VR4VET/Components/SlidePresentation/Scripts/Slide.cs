020ce2c81c466be01138cc6364bd4b79e864035f
Emil Aron Andresen Mathiesen <emilmathie@yahoo.no>
2024-12-16 13:31:41 +0100

    feat: added many control features
    - Audio volume can be adjusted or muted.
    - Automatic slideshow interval can be adjusted.
    - Slide presentation can be turned on or off (this pauses video).
    - Audio Source behaves properly in 3D space (volume tapers off etc).
    - Now supports visualisation of audio volume bar if image is provided.
    - A new example control UI interface showcasing most features.
    
    Refs: #362

--- Assets/VR4VET/Components/SlidePresentation/Scripts/Slide.cs
+++ Assets/VR4VET/Components/SlidePresentation/Scripts/Slide.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -8,7 +6,7 @@ public class Slide : MonoBehaviour
     [Tooltip("Place the slide texture here after setting texture type to 'Sprite (2D and UI)'.")]
     public Sprite SlideTexture;
 
-    [Tooltip("Place a video clip here if it should play when this slide is displayed.")]
+    [Tooltip("OPTIONAL: Place a video clip here if it should play when this slide is displayed.")]
     public VideoClip VideoClip;
 
     [Tooltip("The position of the video clip offset from the center of slide.")]
--- Assets/VR4VET/Components/SlidePresentation/Scripts/SlidePresentation.cs
+++ Assets/VR4VET/Components/SlidePresentation/Scripts/SlidePresentation.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -13,8 +12,21 @@ public class SlideChanged : UnityEvent<int>
 
 public class SlidePresentation : MonoBehaviour
 {
-    [Tooltip("Will change slides every 10 seconds.")]
-    [SerializeField] private bool AutomaticSlideShow = false;
+    [Tooltip("Whether or not the slide presentation is powered on.\n" +
+             "Method PowerOn() will need to be called when presentation should start if this is set false.")]
+    [SerializeField] private bool PoweredOn = true;
+
+    [Tooltip("Will change slides every specified second.")]
+    [SerializeField] private bool AutomaticSlideshow = false;
+
+    [Tooltip("Amount of seconds that will pass before slides are changed when Automatic Slideshow is enabled.")]
+    [SerializeField] private float AutomaticSlideshowInterval = 10f;
+
+    [Tooltip("Enable to prevent video clips from playing audio.")]
+    [SerializeField] private bool MuteAudio = false;
+
+    [Tooltip("OPTIONAL: Place an image component with Image Type set to 'Filled', Fill Method to 'Vertical', and Fill Origin to 'Bottom' here. The component will function as a volume bar and display the current audio volume.")]
+    [SerializeField] private Image VolumeBar;
 
     [Tooltip("Place instances of the Slide prefab here. These are the slides that will be displayed.")]
     [SerializeField] private List<GameObject> Slides = new();
@@ -23,10 +35,11 @@ public class SlidePresentation : MonoBehaviour
     private int _imageIndex = 0;
     private RawImage _videoTexture;
     private VideoPlayer _videoPlayer;
+    private AudioSource _audioSource;
 
     public SlideChanged m_SlideChanged;
-    public UnityEvent ToggledOff;
-    public UnityEvent ToggledOn;
+    public UnityEvent OnPowerOff;
+    public UnityEvent OnPowerOn;
 
     // Start is called before the first frame update
     void Start()
@@ -34,20 +47,23 @@ public class SlidePresentation : MonoBehaviour
         if (m_SlideChanged != null)
             m_SlideChanged = new SlideChanged();
 
-        if (ToggledOff != null)
-            ToggledOff = new UnityEvent();
+        if (OnPowerOff != null)
+            OnPowerOff = new UnityEvent();
 
-        if (ToggledOn != null)
-            ToggledOn = new UnityEvent();
+        if (OnPowerOn != null)
+            OnPowerOn = new UnityEvent();
 
         _videoTexture = GetComponentInChildren<RawImage>();
         _videoPlayer = _videoTexture.GetComponentInChildren<VideoPlayer>();
+        _videoPlayer.waitForFirstFrame = true;
+
+        _audioSource = _videoPlayer.GetComponent<AudioSource>();
         _targetImage = GetComponentInChildren<Image>();
 
         SetSlide(0);
-        if (AutomaticSlideShow)
-            InvokeRepeating(nameof(NextSlide), 10, 10);
-
+        SetAutomaticSlideShow(AutomaticSlideshow);
+        Mute(MuteAudio);
+        AdjustVolumeBar();
     }
 
     public void NextSlide()
@@ -64,16 +80,102 @@ public class SlidePresentation : MonoBehaviour
         m_SlideChanged.Invoke(_imageIndex);
     }
 
-    public void ToggleOff()
+    public void PowerOff()
     {
+        if (!PoweredOn)
+            return;
+
+        if (AutomaticSlideshow && IsInvoking(nameof(NextSlide)))
+            CancelInvoke(nameof(NextSlide));
+
+
         _targetImage.color = Color.clear;
-        ToggledOff.Invoke();
+        if (_videoTexture.enabled)
+        {
+            _videoPlayer.Pause();
+            _videoTexture.color = Color.clear;
+        }
+
+        PoweredOn = false;
+        OnPowerOff.Invoke();
     }
 
-    public void ToggleOn()
+    public void PowerOn()
     {
+        if (PoweredOn)
+            return;
+
+        if (AutomaticSlideshow && !IsInvoking(nameof(NextSlide)))
+            InvokeRepeating(nameof(NextSlide), 10, 10);
+
         _targetImage.color = Color.white;
-        ToggledOn.Invoke();
+        if (_videoTexture.enabled)
+        {
+            _videoPlayer.Play();
+            _videoTexture.color = Color.white;
+        }
+
+        PoweredOn = true;
+        OnPowerOn.Invoke();
+    }
+
+    public void TogglePower()
+    {
+        if (PoweredOn)
+            PowerOff();
+        else
+            PowerOn();
+    }
+
+    public void SetAutomaticSlideShow(bool enable)
+    {
+        AutomaticSlideshow = enable;
+        if (enable)
+        {
+            if (!IsInvoking(nameof(NextSlide)))
+                InvokeRepeating(nameof(NextSlide), 10, 10);
+        }
+        else
+        {
+            if (IsInvoking(nameof(NextSlide)))
+                CancelInvoke(nameof(NextSlide));
+        }
+    }
+
+    public void ToggleAutomaticSlideShow()
+    {
+        AutomaticSlideshow = !AutomaticSlideshow;
+        SetAutomaticSlideShow(AutomaticSlideshow);
+    }
+
+    public void Mute(bool mute)
+    {
+        MuteAudio = mute;
+        _audioSource.mute = mute;
+    }
+
+    public void ToggleMute()
+    {
+        MuteAudio = !MuteAudio;
+        _audioSource.mute = MuteAudio;
+    }
+
+    public void VolumeUp()
+    {
+        _audioSource.volume += .125f;
+        AdjustVolumeBar();
+    }
+
+    public void VolumeDown()
+    {
+        _audioSource.volume -= .125f;
+        AdjustVolumeBar();
+    }
+
+    private void AdjustVolumeBar()
+    {
+        if (VolumeBar != null)
+            VolumeBar.fillAmount = _audioSource.volume;
     }
 
     private void SetSlide(int index)
@@ -83,11 +185,11 @@ public class SlidePresentation : MonoBehaviour
 
         if (slide.VideoClip != null)
         {
-            _videoTexture.enabled = true;
             _videoPlayer.clip = slide.VideoClip;
+            _videoPlayer.frame = 0;
             _videoTexture.transform.localPosition = slide.VideoClipPosition;
             _videoTexture.transform.localScale = slide.VideoClipScale * new Vector3(1, 1, 0);
-            _videoPlayer.frame = 0;
+            _videoTexture.enabled = true;
             _videoPlayer.Play();
         }
         else
